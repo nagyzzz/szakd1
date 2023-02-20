a48ec9f13d5700a3cb6658f98231d221e4dd9616
nagyzzz <nagyzzz1977@gmail.com>
2023-02-20 09:23:30 +0100

    main átírása template alapján

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+import sys
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui as qtg
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from nmap_window import Ui_MainWindow
+from main_window_2 import Ui_MainWindow
+
+class MainWindow(qtw.QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Your code will go here
+
+        # Your code ends here
+        self.show()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = qtw.QApplication(sys.argv)
+    w = MainWindow()
+    sys.exit(app.exec_())
